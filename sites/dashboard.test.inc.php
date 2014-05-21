4b5127c3ace30e587e327d8e92c736328424afbd
Robert <rm@flipez.de>
2014-05-21 16:50:57 +0200

    added new test and homework table

--- sites/dashboard.test.inc.php
+++ sites/dashboard.test.inc.php
@@ -6,3 +6,50 @@
  * Time: 16:32
  * To change this template use File | Settings | File Templates.
  */
+?>
+<br />
+<h1>Übersicht der anstehenden Arbeiten</h1>
+<table>
+    <thead>
+        <th>ID</th>
+        <th>Fach</th>
+        <th>Datum</th>
+        <th>Wertigkeit</th>
+    </thead>
+    <tr>
+        <td>1
+        </td>
+        <td>
+            EBA-Java
+        </td>
+        <td>
+            24.12.2014
+        </td>
+        <td>
+            doppelt
+        </td>
+    </tr>
+</table>
+<h1>Übersicht der anstehenden Arbeiten</h1>
+<table>
+    <thead>
+    <th>ID</th>
+    <th>Fach</th>
+    <th>Beschreibung</th>
+    <th>Datum</th>
+    </thead>
+    <tr>
+        <td>
+            1
+        </td>
+        <td>
+            Englisch
+        </td>
+        <td>
+            Vortrag auf Englisch
+        </td>
+        <td>
+            24.12.2014
+        </td>
+    </tr>
+</table>
