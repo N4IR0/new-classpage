fc8e179c3d3d05a86ec6a873882ce8d9bc72e43f
N4IR0 <nairo@flohome.de>
2014-05-22 21:41:30 +0200

    Added default group for homework overview

--- sites/dashboard.home.inc.php
+++ sites/dashboard.home.inc.php
@@ -1,2 +1,12 @@
+<?php
+	if (isset($_COOKIE["group"])) {
+		if (!$_SERVER['HTTPS']) {
+			$proto = "http";
+		} else {
+			$proto = "https";
+		}
+		header('Location: '.$proto.'://'.$_SERVER["SERVER_NAME"].'/dashboard/group'.$_COOKIE["group"]);
+	}
+?>
 <b>Herzlich Willkommen zurück!</b><br /><br />
 <p>Hier findest du eine Übericht der aktuellen Hausaufgaben und Arbeiten. Bitte wähle links deine Gruppe aus.</p>
