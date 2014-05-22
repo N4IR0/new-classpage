7840588b6070e1b4a30315c1611cf474aff10ebf
Robert <rm@flipez.de>
2014-05-22 21:26:41 +0200

    improved homework overview, added homework detail

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -6,51 +6,53 @@
 	function info($txt) {
 		echo "<div class=correct>$txt</div>";
 	}
-function getHomework($group, $date = NULL) {
-    $table = NULL;
-    $timestamp = time();
-    $query= "SELECT `id`, `subject`, `description`, `date`, `notify_date` FROM `homework_$group` WHERE `date` >= '$timestamp' LIMIT 10";
+function getHomework($group, $id = NULL, $date = NULL) {
 
-    $result= mysql_query($query);
-
-    while($row = mysql_fetch_assoc($result)) {
-        $table.="<tr>";
-        $table.="<td>";
-        $table.=$row['id'];
-        $table.="</td>";
-        $table.="<td>";
-        $table.=$row['subject'];
-        $table.="</td>";
-        $table.="<td>";
-        $table.=$row['description'];
-        $table.="</td>";
-        $table.="<td>";
-        $table.=date("d.m.Y",$row['date']);
-        $table.="</td>";
-
-        $diff = $row['date'] - $timestamp;
-        $diff = floor($diff / (3600*24));
-
-
-        if ($diff <= "2") {
-            $table.="<td style='color: #FF0000;'>";
-            $table.=$diff." Tage";
+    if (!empty($id)) {
+        $query= "SELECT `id`, `subject`, `description`, `date`, `notify_date` FROM `homework` WHERE `id` = '$id' LIMIT 1";
+        $result= mysql_query($query);
+        $result = mysql_fetch_assoc($result);
+        return $result;
+    } else {
+        $table = NULL;
+        $timestamp = time();
+        $query= "SELECT `id`, `subject`, `description`, `date`, `notify_date` FROM `homework` WHERE `date` >= '$timestamp' AND `group` = '$group' LIMIT 10";
+        $result= mysql_query($query);
+        while($row = mysql_fetch_assoc($result)) {
+            $table.="<tr>";
+            $table.= "<td style='text-decoration: none;'>";
+            $table.="<a href='/dashboard/homework/show/".$row['id']."'>".$row['id']."</a>";
             $table.="</td>";
-        } elseif ($diff <= "5") {
-            $table.= "<td style='color: #ffaf00;'>";
-            $table.=$diff." Tage";
+            $table.="<td>";
+            $table.=$row['subject'];
             $table.="</td>";
-        } else {
-            $table.="<td style='color: #008000;'>";
-            $table.=$diff." Tage";
+            $table.="<td>";
+            $table.=$row['description'];
             $table.="</td>";
+            $table.="<td>";
+            $table.=date("d.m.Y",$row['date']);
+            $table.="</td>";
+            $diff = $row['date'] - $timestamp;
+            $diff = floor($diff / (3600*24));
+            if ($diff <= "2") {
+                $table.="<td style='color: #FF0000;'>";
+                $table.=$diff." Tage";
+                $table.="</td>";
+            } elseif ($diff <= "5") {
+                $table.= "<td style='color: #ffaf00;'>";
+                $table.=$diff." Tage";
+                $table.="</td>";
+            } else {
+                $table.="<td style='color: #008000;'>";
+                $table.=$diff." Tage";
+                $table.="</td>";
+            }
+             $table.="</tr>";
         }
-
-
-
-        $table.="</tr>";
+        return $table;
     }
-    return $table;
+
+
 }
 	function getMonday($timestamp=NULL) {
 		if (empty($timestamp)) {
---
+++ sites/dashboard.homework.inc.php
@@ -0,0 +1,42 @@
+<?php
+
+if (isset($_GET["id1"]) && isset($_GET["id2"])) {
+    $id1 = $_GET["id1"];
+    $id2 = $_GET["id2"];
+
+    $data = getHomework(NULL,$id2);
+
+    $html = "<h3>Details zur Hausaufgabe Nr. ".$_GET["id2"]."</h3>";
+    $html.= "<table>";
+    $html.= "<tr>";
+    $html.="<td>Hausaufgabe Nr.</td>";
+    $html.="<td>".$data['id']."</td>";
+    $html.= "</tr>";
+    $html.= "<tr>";
+    $html.="<td>Fach</td>";
+    $html.="<td>".$data['subject']."</td>";
+    $html.= "</tr>";
+    $html.= "<tr>";
+    $html.="<td>Aufgabe</td>";
+    $html.="<td>".$data['description']."</td>";
+    $html.= "</tr>";
+    $html.= "<tr>";
+    $html.="<td>Beschreibung lang</td>";
+    $html.="<td>n/A</td>";
+    $html.= "</tr>";
+    $html.= "<tr>";
+    $html.="<td>Datum</td>";
+    $html.="<td>".$data['date']."</td>";
+    $html.= "</tr>";
+    $html.= "<tr>";
+    $html.="<td>Benachichtigt am</td>";
+    $html.="<td>".$data['notify_date']."</td>";
+    $html.= "</tr>";
+    $html.= "<tr>";
+    $html.="<td>Lösung / Beispiel</td>";
+    $html.="<td>Das könnte ein LINK sein</td>";
+    $html.= "</tr>";
+    $html.= "</table>";
+    echo $html;
+}
+?>
