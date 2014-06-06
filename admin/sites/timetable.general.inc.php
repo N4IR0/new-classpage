ba5e99348dd4da9027c91827b9020a2a8d0fc3d4
Florian Voit <nairo@flohome.de>
2014-06-06 23:38:01 +0200

    Fixed some issues

--- admin/sites/timetable.general.inc.php
+++ admin/sites/timetable.general.inc.php
@@ -10,19 +10,23 @@
 	echo "<h3>Schulfreie Tage</h3>";
 	$result = getSubstitution(2);
 	echo "<table id='table'>";
-		echo "<tr>";
-			echo "<th>Datum</th>";
-			echo "<th>Grund</th>";
-			echo "<th>Bearbeiten</th>";
-			echo "<th>Löschen</th>";
-		echo "</tr>";
-		while ($row = mysql_fetch_assoc($result)) {
+		echo "<thead>";
 			echo "<tr>";
-				echo "<td>".date("d.m.Y", $row["date"])."</td>";
-				echo "<td>".$row["lesson"]."</td>";
-				echo "<td><a href=".$CONFIG["website"]["admin_path"]."timetable/general/edit/".$row["id"].">x</a></td>";
-				echo "<td><a href=".$CONFIG["website"]["admin_path"]."timetable/general/delete/".$row["id"].">x</a></td>";
+				echo "<th>Datum</th>";
+				echo "<th>Grund</th>";
+				echo "<th>Bearbeiten</th>";
+				echo "<th>Löschen</th>";
 			echo "</tr>";
-		}
+		echo "</thead>";
+		echo "<tbody>";
+			while ($row = mysql_fetch_assoc($result)) {
+				echo "<tr>";
+					echo "<td>".date("d.m.Y", $row["date"])."</td>";
+					echo "<td>".$row["lesson"]."</td>";
+					echo "<td><a href=".$CONFIG["website"]["admin_path"]."timetable/general/edit/".$row["id"].">x</a></td>";
+					echo "<td><a href=".$CONFIG["website"]["admin_path"]."timetable/general/delete/".$row["id"].">x</a></td>";
+				echo "</tr>";
+			}
+		echo "</tbody>";
 	echo "</table>";
 ?>
